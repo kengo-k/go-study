911a07cc8c92a8699140f3d088a38ef699e9d55e
kengo-k <kurobanekengo@gmail.com>
2022-09-09 12:57:01 +0900

    01/flag: flagを使用してコマンドラインオプションを処理するサンプルを追加

---
+++ app/src/cmd/01/flags.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "flag"
+  "log"
+)
+
+var (
+  // 第一引数: オプション名
+  // 第二引数: デフォルト値
+  // 第三引数: 説明
+  //
+  // 引数は以下のように指定する
+  // -s hello
+  // -s=hello
+  // -s="hello" -i 200
+  FlagStr = flag.String("s", "default", "文字列フラグ")
+  FlagInt = flag.Int("i", -1, "数値フラグ")
+)
+
+func main() {
+  // 想定外のフラグを渡すとusageを表示して処理を終了する
+  flag.Parse()
+  log.Println(*FlagStr)
+  log.Println(*FlagInt)
+  // Argsを呼び出すとフラグ指定されていない残りの引数を取得できる
+  log.Println(flag.Args())
+}
